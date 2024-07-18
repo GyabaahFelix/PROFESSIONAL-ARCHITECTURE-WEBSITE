1403e593341c6245fd6ed68f9b6b9e5c3d63daa3
Felixic99 <fgyabaah003@st.ug.edu.gh>
2024-07-18 14:35:23 +0000

    Add files via upload

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <header id='header'>
+      <div className="container full__height"></div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Teams/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Teams.css"
+
+const Teams = () => {
+  return (
+    <div>Teams</div>
+  )
+}
+
+export default Teams
